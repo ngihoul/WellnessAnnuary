ef891c8057f9eb64b58a4c556a5dc4da647455ae
Nicolas Gihoul <ngihoul@hotmail.com>
2021-12-14 20:30:18 +0100

    Creating Newsletter Entity

---
+++ annuary/src/Entity/Newsletter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsletterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsletterRepository::class)
+ */
+class Newsletter
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $publishedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $PDFDocument;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPDFDocument(): ?string
+    {
+        return $this->PDFDocument;
+    }
+
+    public function setPDFDocument(string $PDFDocument): self
+    {
+        $this->PDFDocument = $PDFDocument;
+
+        return $this;
+    }
+}
