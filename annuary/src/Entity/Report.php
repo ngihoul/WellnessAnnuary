19066c69f39cf9340f8a5bb88410e6aa3806d5ec
Nicolas Gihoul <ngihoul@hotmail.com>
2021-12-14 20:38:57 +0100

    Creating Report Entity

---
+++ annuary/src/Entity/Report.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReportRepository::class)
+ */
+class Report
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $reportedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getReportedAt(): ?\DateTimeInterface
+    {
+        return $this->reportedAt;
+    }
+
+    public function setReportedAt(\DateTimeInterface $reportedAt): self
+    {
+        $this->reportedAt = $reportedAt;
+
+        return $this;
+    }
+}
