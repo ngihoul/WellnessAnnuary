ccab9d80c59fc071f27d376d537efafab0495d70
Nicolas Gihoul <ngihoul@hotmail.com>
2021-12-14 19:21:35 +0100

    Creating Promotion Entity

---
+++ annuary/src/Entity/Promotion.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PromotionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PromotionRepository::class)
+ */
+class Promotion
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $PDFDocument;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $startAt;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $endAt;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $displayedFrom;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $displayedUntil;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPDFDocument(): ?string
+    {
+        return $this->PDFDocument;
+    }
+
+    public function setPDFDocument(?string $PDFDocument): self
+    {
+        $this->PDFDocument = $PDFDocument;
+
+        return $this;
+    }
+
+    public function getStartAt(): ?\DateTimeInterface
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(\DateTimeInterface $startAt): self
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeInterface
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(\DateTimeInterface $endAt): self
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getDisplayedFrom(): ?\DateTimeInterface
+    {
+        return $this->displayedFrom;
+    }
+
+    public function setDisplayedFrom(\DateTimeInterface $displayedFrom): self
+    {
+        $this->displayedFrom = $displayedFrom;
+
+        return $this;
+    }
+
+    public function getDisplayedUntil(): ?\DateTimeInterface
+    {
+        return $this->displayedUntil;
+    }
+
+    public function setDisplayedUntil(\DateTimeInterface $displayedUntil): self
+    {
+        $this->displayedUntil = $displayedUntil;
+
+        return $this;
+    }
+}
