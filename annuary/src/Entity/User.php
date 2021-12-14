e6f3cfaf57d806e48c5fc2f50e5259dae8a9151c
Nicolas Gihoul <ngihoul@hotmail.com>
2021-12-14 19:52:59 +0100

    Creating User Entity

---
+++ annuary/src/Entity/User.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $addressNumber;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $addressStreet;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $registeredOn;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $unsuccessfulAttempts;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $banned;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $registrationConfirmed;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getAddressNumber(): ?string
+    {
+        return $this->addressNumber;
+    }
+
+    public function setAddressNumber(string $addressNumber): self
+    {
+        $this->addressNumber = $addressNumber;
+
+        return $this;
+    }
+
+    public function getAddressStreet(): ?string
+    {
+        return $this->addressStreet;
+    }
+
+    public function setAddressStreet(string $addressStreet): self
+    {
+        $this->addressStreet = $addressStreet;
+
+        return $this;
+    }
+
+    public function getRegisteredOn(): ?\DateTimeInterface
+    {
+        return $this->registeredOn;
+    }
+
+    public function setRegisteredOn(\DateTimeInterface $registeredOn): self
+    {
+        $this->registeredOn = $registeredOn;
+
+        return $this;
+    }
+
+    public function getUnsuccessfulAttempts(): ?int
+    {
+        return $this->unsuccessfulAttempts;
+    }
+
+    public function setUnsuccessfulAttempts(?int $unsuccessfulAttempts): self
+    {
+        $this->unsuccessfulAttempts = $unsuccessfulAttempts;
+
+        return $this;
+    }
+
+    public function getBanned(): ?bool
+    {
+        return $this->banned;
+    }
+
+    public function setBanned(bool $banned): self
+    {
+        $this->banned = $banned;
+
+        return $this;
+    }
+
+    public function getRegistrationConfirmed(): ?bool
+    {
+        return $this->registrationConfirmed;
+    }
+
+    public function setRegistrationConfirmed(bool $registrationConfirmed): self
+    {
+        $this->registrationConfirmed = $registrationConfirmed;
+
+        return $this;
+    }
+}
