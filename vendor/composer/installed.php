0d052aae5c61471ba56137ac94e9969441358da4
Nicolas Gihoul <ngihoul@hotmail.com>
2021-12-16 19:07:14 +0100

    Updating Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4e51e7870f7cf032bf50aa6b4bd89dbd05a4401c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4e51e7870f7cf032bf50aa6b4bd89dbd05a4401c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
