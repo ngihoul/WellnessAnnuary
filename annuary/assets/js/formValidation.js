57b8d38176d87d7af0752c3a14938b4a0d1544ad
Nicolas Gihoul <ngihoul@hotmail.com>
2022-01-30 12:31:42 +0100

    Refactoring Javascript of Registration form

--- annuary/assets/js/formValidation.js
+++ annuary/assets/js/formValidation.js
@@ -2,108 +2,91 @@
 // *** Form validation *** //
 // *********************** //
 
-// ** Variables & constants ** //
+// * Variables * //
 const providerInputs = document.forms['provider'].getElementsByTagName('input') || document.forms['customer'].getElementsByTagName('input');
 const websiteField = document.getElementById('provider_website');
 const phoneNumberField = document.getElementById('provider_phoneNumber');
 const VTANumberField = document.getElementById('provider_VTANumber');
 const emailField = document.getElementById('provider_user_email');
-const postCodeField = document.getElementById('provider_user_postCode');
-const passwordField = document.getElementById('provider_user_password');
-const passwordConfirmField = document.getElementById('provider_user_confirmPassword');
+const postCodeField = document.getElementById('provider_user_postCode') || document.getElementById('customer_user_postCode');
+const passwordField = document.getElementById('provider_user_password') || document.getElementById('customer_user_password');
+const passwordConfirmField = document.getElementById('provider_user_confirmPassword') || document.getElementById('customer_user_confirmPassword');
 
+// * Constants * //
+const PREFIX_BELGIUM = '+32';
+const HTML_CLASS_VALIDATED = 'validated';
+const HTML_CLASS_NOT_VALIDATED = 'notValidated';
+const HTML_CLASS_ERROR_MSG = 'error-message';
+const PHONENUMBER_MIN_LENGTH = 12;
+const PHONENUMBER_MAX_LENGTH = 13;
+const VTANUMBER_MIN_LENGTH = 12;
+const VTANUMBER_MAX_LENGTH = 13;
+const POSTCODE_LENGTH = 4;
+const PASSWORD_MIN_LENGTH = 7;
+const PASSWORD_MAX_LENGTH = 255;
+
+// * Messages * //
 const MSG_BLANK = 'Veuillez compléter ce champ';
 const MSG_NOT_WEBSITE = 'Format attendu : www.exemple.be';
 const MSG_NOT_PHONENUMBER = 'Format attendu : +32 X XXX XX XX ou +32 XXX XX XX XX';
-const MSG_NOT_VTANUMBER = 'Format attendu : BE0 X XXX XXX XXX ou BE X XXX XXX XXXX';
+const MSG_NOT_VTANUMBER = 'Format attendu : BE 0 XXX XXX XXX ou BE 0 XXX XXX XXXX';
 const MSG_NOT_EMAIL = 'Format attendu : exemple@exemple.be';
 const MSG_NOT_POSTCODE = 'Format attendu : XXXX';
-const MSG_NOT_PASSWORD = 'Le mot de passe doit être de minimum 7 caractères et doit contenir : <br> - une lettre majuscule <br> - une lettre minuscule <br> - un chiffre <br> - un caractère spécial (? ou ! ou @)';
+const MSG_NOT_PASSWORD = 'Le mot de passe doit être de minimum 7 caractères et doit contenir au moins : <br> - une lettre majuscule <br> - une lettre minuscule <br> - un chiffre <br> - un caractère spécial (? ou ! ou @)';
 const MSG_NOT_SIMILAR = 'Les mots de passe ne sont pas identiques';
-
-const PREFIX_BELGIUM = '+32';
 
 // ** Functions ** //
 // * Add style if field is validated or not * //
-const fieldValidated = field => field.className = 'validated';
-const fieldNotValidated = field => field.className = 'notValidated';
+const fieldValidated = field => {
+    field.className = HTML_CLASS_VALIDATED;
+    if(field.nextSibling) {
+        field.nextSibling.remove();
+    }
+}
+
+const fieldNotValidated = (field, message) => {
+    field.className = HTML_CLASS_NOT_VALIDATED;
+    if(!field.nextSibling) {
+        addMessage(field, message);
+    }
+}
 
 // * Add Message * //
-const addMessage = (field, message) => {
+const addMessage = (field, message, htmlClass = HTML_CLASS_ERROR_MSG) => {
     let newElement = document.createElement('p');
     newElement.innerHTML = message;
-    newElement.className = 'error-message';
+    newElement.className = htmlClass;
     field.parentNode.insertBefore(newElement, field.nextSibling);
 }
 
 // ** Check blank fields ** //
-// * Format field if empty
-const formatIfBlank = (field) => {
-    if(blank(field)) {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_BLANK);
-        }
-    } else {
-        fieldValidated(field);
-        field.nextSibling.remove();
-    }
-}
+const formatIfNotBlank = field => isBlank(field) ? fieldNotValidated(field, MSG_BLANK) : fieldValidated(field);
 
-// * Check if input is empty
-const blank = field => field.value.length < 1 ? true : false;
+const isBlank = field => field.value.length < 1 ? true : false;
 
 // ** Check phoneNumber ** //
-// * Format field if not a valid phoneNumber * /
-const formatFieldIfNotPhoneNumber = field => {
+const formatIfNotPhoneNumber = field => {
     // Delete whitespaces
     field.value = escapeSpaces(field.value);
 
     // If starts with 0 replace it by +32
-    if(startWithZero(field.value)) {
-        field.value = field.value.replace('0', PREFIX_BELGIUM);
-    }
+    field.value = startWithZero(field.value) ? field.value.replace('0', PREFIX_BELGIUM) : field.value;
 
     // If not correspond to a belgian phonenumber, format field
-    if(!isBelgianPhoneNumber(field)) {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_NOT_PHONENUMBER);
-        }
-    } else {
-        fieldValidated(field);
-        if(field.nextSibling) {
-            field.nextSibling.remove();
-        }
-    }
+    isBelgianPhoneNumber(field) ? fieldValidated(field) : fieldNotValidated(field, MSG_NOT_PHONENUMBER);
 }
 
-// * Delete whitespaces in string * //
 const escapeSpaces = string => string.replace(/\s/g, '');
 
-// * check if string start with 0 * //
 const startWithZero = string => string.charAt(0) == 0 ? true : false;
 
-// * Check if string start with +32 * //
 const isBelgianPhoneNumber = field => {
-    return field.value.startsWith(PREFIX_BELGIUM) && field.value.length >= 11 && field.value.length <= 12 ? true : false;
+    return field.value.startsWith(PREFIX_BELGIUM) && field.value.length >= PHONENUMBER_MIN_LENGTH && field.value.length <= PHONENUMBER_MAX_LENGTH ? true : false;
 }
 
 // ** Check if website ** //
-// * Format field if not a website * //
-const formatIfNotWebsite = (field) => {
-    if(!isWebsite(field)) {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_NOT_WEBSITE);
-        }
-    } else {
-        fieldValidated(field);
-        field.nextSibling.remove();
-    }
-}
+const formatIfNotWebsite = field => isWebsite(field) ? fieldValidated(field) : fieldNotValidated(field, MSG_NOT_WEBSITE);
 
-// * Check if website field is a url * //
 const isWebsite = field => {
     let expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     let regex = new RegExp(expression);
@@ -112,41 +95,24 @@ const isWebsite = field => {
 }
 
 // ** Check if VTA Number ** //
-// * Check if is a valid Belgian VTA Number * /
 const formatIfNotBelgianVTANumber = field => {
-    field.value = escapeSpaces(field.value);
+    // Delete spaces & transform to upperCase
+    field.value = escapeSpaces(field.value).toUpperCase();
 
-    if(startWithBE0(field) && (field.value.length == 12 || field.value.length == 13)) {
+    if(startsWithBE0(field) && (field.value.length == VTANUMBER_MIN_LENGTH || field.value.length == VTANUMBER_MAX_LENGTH)) {
         fieldValidated(field);
-        if(field.nextSibling) {
-            field.nextSibling.remove();
-        }
     } else {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_NOT_VTANUMBER);
-        }
+        fieldNotValidated(field, MSG_NOT_VTANUMBER);
     }
 }
 
-// * Check if starts with BE0 * /
-const startWithBE0 = field => field.value.startsWith('BE0') ? true : false;
+const startsWithBE0 = field => {
+    return field.value.startsWith('BE0') ? true : false;
+}
 
 // ** Check if email address ** //
-const formatIfNotEmail = (field) => {
-    if(!isEmail(field)) {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_NOT_EMAIL);
-        }
-    } else {
-        fieldValidated(field);
-        if(field.nextSibling) {
-            field.nextSibling.remove();
-        }
-    }
-}
-// * check if value of field is a valid email * /
+const formatIfNotEmail = field => isEmail(field) ? fieldValidated(field) : fieldNotValidated(field, MSG_NOT_EMAIL);
+
 const isEmail = field => {
     let expression = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     let regex = new RegExp(expression);
@@ -155,64 +121,28 @@ const isEmail = field => {
 }
 
 // ** Check if postCode is valid ** //
-const formatIfNotPostCode = (field) => {
-    if(!isBelgianPostCode(field)) {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_NOT_POSTCODE);
-        }
-    } else {
-        fieldValidated(field);
-        if(field.nextSibling) {
-            field.nextSibling.remove();
-        }
-    }
-}
+const formatIfNotPostCode = field => isBelgianPostCode(field) ? fieldValidated(field) : fieldNotValidated(field, MSG_NOT_POSTCODE);
 
-const isBelgianPostCode = field => field.value.length == 4 ? true : false;
+const isBelgianPostCode = field => field.value.length == POSTCODE_LENGTH ? true : false;
 
 // ** Check password - One uppercase letter, one lowercase letter, one digit and one special chars ** /
-const formatIfNotPassword = (field) => {
-    if(!isPassword(field)) {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_NOT_PASSWORD);
-        }
-    } else {
-        fieldValidated(field);
-        if(field.nextSibling) {
-            field.nextSibling.remove();
-        }
-    }
-}
+const formatIfNotPassword = field => isPassword(field) ? fieldValidated(field) : fieldNotValidated(field, MSG_NOT_PASSWORD);
 
 const isPassword = field => {
     let expression = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!@.*\s).{7,255}$/;
     let regex = new RegExp(expression);
 
-    return field.value.match(regex) && field.value.length >= 7 && field.value.length <= 255 ? true : false;
+    return field.value.match(regex) && field.value.length >= PASSWORD_MIN_LENGTH && field.value.length <= PASSWORD_MAX_LENGTH ? true : false;
 }
 
 // ** check if confirm password is similar to password ** ///
-const formatIfPasswordsNotSimilar = (field) => {
-    if(passwordIsSimilar(field)) {
-        fieldNotValidated(field);
-        if(!field.nextSibling) {
-            addMessage(field, MSG_NOT_SIMILAR);
-        }
-    } else {
-        fieldValidated(field);
-        if(field.nextSibling) {
-            field.nextSibling.remove();
-        }
-    }
-}
+const formatIfPasswordsNotSimilar = field => passwordIsSimilar(field) ? fieldValidated(field) : fieldNotValidated(field, MSG_NOT_SIMILAR);
 
 const passwordIsSimilar = field => {
-    return field.value != passwordField.value ? false : true;
+    return field.value == passwordField.value ? true : false;
 }
 
-// *** Scripts *** //
+// ** Scripts ** //
 // * If field is blank * //
 for(let input of providerInputs) {
     if(input != websiteField &&
@@ -222,8 +152,9 @@ for(let input of providerInputs) {
         input != postCodeField &&
         input != passwordField &&
         input != passwordConfirmField ) {
+
             input.addEventListener('focusout', () => {
-                formatIfBlank(input);
+                formatIfNotBlank(input);
         });
     }
 }
@@ -233,7 +164,7 @@ websiteField.addEventListener('focusout', () => {
 });
 
 phoneNumberField.addEventListener('focusout', () => {
-    formatFieldIfNotPhoneNumber(phoneNumberField);
+    formatIfNotPhoneNumber(phoneNumberField);
 });
 
 VTANumberField.addEventListener('focusout', () => {
@@ -248,12 +179,17 @@ postCodeField.addEventListener('focusout', () => {
     formatIfNotPostCode(postCodeField);
 });
 
+// Add message to password
+addMessage(passwordField, MSG_NOT_PASSWORD, 'info-message');
+
 passwordField.addEventListener('focusout', () => {
     formatIfNotPassword(passwordField);
 });
 
 passwordConfirmField.addEventListener('focusout', () => {
-    formatIfPasswordsNotSimilar(passwordConfirmField);
+    if(!isBlank(passwordConfirmField)) {
+        formatIfPasswordsNotSimilar(passwordConfirmField);
+    }
 });
 
 
--- annuary/assets/js/registration.js
+++ annuary/assets/js/registration.js
@@ -2,28 +2,24 @@
 // **** Registration Form **** //
 // *************************** //
 
-// ***************************** //
-// *** Variables & constants *** //
-// ***************************** //
+// ** Variables & constants ** //
+// * Variables * /
 const postCodeField = document.getElementById('provider_user_postCode') || document.getElementById('customer_user_postCode');
 const localityField = document.getElementById('provider_user_locality') || document.getElementById('customer_user_locality');
 
-// ***************** //
-// *** Functions *** //
-// ***************** //
+// * Constants * //
+const POSTCODE_LENGTH  = 4;
+const AJAX_URL = '/search/where/?w=';
 
-// **************** //
-// ** Localities ** //
-// **************** //
-
+// ** Functions ** //
 // * Capitalize strings * //
-const capitalizeFirstLetter = (string) => {
+const capitalizeFirstLetter = string => {
     string = string.toLowerCase();
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // * Display Localities if there is a change on an input * //
-const displayLocalitiesOnInputEvent = (field) => {
+const displayLocalitiesOnInputEvent = field => {
     field.addEventListener('input', () => {
         if(field.value.length > 0) {
             fetchAndDisplayLocalities(field);
@@ -34,15 +30,15 @@ const displayLocalitiesOnInputEvent = (field) => {
 }
 
 // * Display localities if input value has already a length value of 4 * //
-const displayLocalitiesIfFieldValueEqualsFour = (field) => {
-    if(field.value.length == 4) {
+const displayLocalitiesIfFieldValueEqualsFour = field => {
+    if(field.value.length == POSTCODE_LENGTH) {
         fetchAndDisplayLocalities(field);
     }
 }
 
 // * Fetch and Display Locailities in appropriate field * //
-const fetchAndDisplayLocalities = (field) => {
-    fetch(`/search/where/?w=${field.value.trim()}`)
+const fetchAndDisplayLocalities = field => {
+    fetch(`${AJAX_URL}${field.value.trim()}`)
     .then(response => response.json())
     .then(data => displayLocalities(localityField, data));
 }
@@ -51,19 +47,11 @@ const fetchAndDisplayLocalities = (field) => {
 const displayLocalities = (localityField, data) => {
     localityField.innerHTML = "";
     data.forEach((element) => {
-        console.log(element);
         localityField.innerHTML += `<option value="${element.id}">${capitalizeFirstLetter(element.locality)}</option>`;
     });
 }
 
-
-
-
-
-// *************** //
-// *** SCRIPTS *** //
-// *************** //
-
+// ** Scripts ** //
 // * Delete all options in Locality <select> * //
 localityField.innerHTML = "";
 
